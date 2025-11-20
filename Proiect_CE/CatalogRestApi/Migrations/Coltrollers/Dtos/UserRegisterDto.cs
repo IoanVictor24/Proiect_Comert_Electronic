c048003d2f64a41ac302b8bd0bf14ced37811412
IoanVictor24 <sarpe.ioan.k4e@student.ucv.ro>
2025-11-20 16:17:07 +0200

    Initial commit

--- Proiect_CE/CatalogRestApi/Migrations/Coltrollers/Dtos/UserRegisterDto.cs
+++
@@ -1,9 +0,0 @@
-namespace CatalogRestApi.Dtos
-{
-    public class UserRegisterDto
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Role { get; set; } = "user"; // Implicit 'user'
-    }
-}
