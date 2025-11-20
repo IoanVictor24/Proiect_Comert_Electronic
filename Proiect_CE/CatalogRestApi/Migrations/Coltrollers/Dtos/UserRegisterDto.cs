f8a70f1ae61026f861f8e3e9059a2a79f04fb3c7
IoanVictor24 <sarpe.ioan.k4e@student.ucv.ro>
2025-11-20 15:00:18 +0200

    Initial project source code

---
+++ Proiect_CE/CatalogRestApi/Migrations/Coltrollers/Dtos/UserRegisterDto.cs
@@ -0,0 +1,9 @@
+namespace CatalogRestApi.Dtos
+{
+    public class UserRegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; } = "user"; // Implicit 'user'
+    }
+}
---
+++ Proiect_CE/CatalogRestApi/Models/Category.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic; // Nevoie pentru ICollection
+
+namespace CatalogRestApi.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        // Proprietate de navigare: EF Core va ști că o Categorie conține mai multe Produse
+        public ICollection<Product> Products { get; set; }
+    }
+}
