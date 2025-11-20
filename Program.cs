ce996bb1daadb9de75f92696e71b47d7a0a72667
IoanVictor24 <sarpe.ioan.k4e@student.ucv.ro>
2025-11-21 01:13:46 +0200

    inceput-frontend

--- Program.cs
+++ Program.cs
@@ -19,7 +19,15 @@ builder.Services.AddControllers();
 // 3. Adaugă serviciul Swagger (pentru testare)
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 // ----------------------------------------------------------------------
 // PASUL 2: CONSTRUIREA APLICAȚIEI
 // ----------------------------------------------------------------------
@@ -48,7 +56,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAll");
 // app.UseAuthorization(); // Îl vom adăuga când implementăm JWT
 
 app.MapControllers(); // Harta rutele Controller-elor (inclusiv AuthController)
