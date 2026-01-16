7f646ebbd99fdb94c1ca6a9092e1970934576494
IoanVictor24 <sarpe.ioan.k4e@student.ucv.ro>
2026-01-16 11:34:30 +0200

    am adaugat wishlist

--- Proiect Backend/Models/Data/ApplicationDbContext.cs
+++ Proiect Backend/Models/Data/ApplicationDbContext.cs
@@ -1,19 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using ProiectCE.Models;
 
+// ATENȚIE: Am schimbat namespace-ul în ProiectCE.Data ca să nu mai dea eroare în Controllers
 namespace ProiectCE.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        // Constructor: Configurează baza de date
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-        // DbSet-urile reprezintă tabelele din baza de date
         public DbSet<User> Users { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+     
     }
 }
---
+++ Proiect Backend/Models/WishlistItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProiectCE.Models
+{
+    public class WishlistItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int UserId { get; set; } // ID-ul utilizatorului care a salvat produsul
+
+        public int ProductId { get; set; } // ID-ul produsului salvat
+
+        [ForeignKey("ProductId")]
+        public Product? Product { get; set; } // Legătura cu tabela de produse
+    }
+}
